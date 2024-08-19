2e4c5b67adc3a3c6584ba54519486bf32d685d0d
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-08-20 01:14:25 +0500

    minor changes

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $site_name = "Lamenta";
 $site_description = "Lamenta - Empowering Resilience Through Knowledge";
 $company_name = "The ShazamBolt8 Wonders";
-$site_address = "https://lementa.shazambolt8/"; //an ending slash is required
-$site_email = "help@lamenta.shazambolt8";
+$site_address = "http://localhost/"; //an ending slash is required
+$site_email = "help@localhost";
 $site_logo = "https://i.postimg.cc/2ytNTndY/logo.png";
 $site_banner = "https://i.postimg.cc/Y0fcQTX5/bitmap.png";
